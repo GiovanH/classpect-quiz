03b84ad63769a219a2611d92b6c640fb4e9faf27
Gio <sethg@ipi.org>
2023-11-25 03:23:55 -0600

    progress

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,55 @@
 <template>
   <div v-for="(aspects, theme) in data.aspect_pairs" :key="theme">
-    <h2 v-text="$t(`theme_${theme}`)" />
-    <p>
-      Which of these two sides of <i>{{ theme }}</i> do you have the <b>strongest</b> connection to?
-    </p>
-    <template v-for="aspect in aspects" :key="aspect">
-      <dt>
-        <label>
-          <input type="radio"
-            :name="aspect"
-            :id="aspect"
-            :value="aspect"
-            v-model="theme_picks[theme]"
-          />
-          <!-- {{ aspect }} -->
-          {{ $t(`aspect_desc.${aspect}`) }}
-        </label>
-      </dt>
-      <!-- <label :for="aspect">
-        <dd class="settingDesc" v-html="$t(`aspect_desc.${aspect}`)" />
-      </label> -->
-    </template>
-    <pre v-text="{k, apair}" />
+    <AspectQuiz
+      :theme="theme"
+      @aspectSet="v => quiz_inputs[theme].aspect = v"
+      @classSet="v => quiz_inputs[theme].class = v"
+      @confidenceSet="v => quiz_inputs[theme].confidence = v"
+    />
   </div>
-  <pre v-text="theme_picks" />
-  <pre v-text="scores" />
+  <ol>
+    <li v-for="(res, theme) in quiz_inputs" :key="theme">
+      {{ res.class }} of {{ res.aspect }}
+    </li>
+  </ol>
+  <pre v-text="quiz_inputs" />
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.row {
+  display: flex;
+}
+.col {
+  display: flex;
+  flex-direction: column;
+}
 </style>
 
 <script>
 
 import data from "./data.yaml";
+import AspectQuiz from "@/components/AspectQuiz.vue"
 
 export default {
   name: 'App',
+  components: { AspectQuiz },
   data: function() {
     return {
       data: data,
-      theme_picks: {}
+      quiz_inputs: {
+
+      }
     }
   },
   computed: {
-    scores() {
-      var scores_ = {}
-      Object.keys(data.aspect_pairs).forEach(theme => {
-        scores_[theme] = {
-          "aspect": this.theme_picks[theme],
-          "score": 0
-        }
-      })
-      return scores_
-    }
   },
   methods: {
 
   },
   created() {
+    Object.keys(data.aspect_pairs).forEach(theme => {
+      this.quiz_inputs[theme] = {}
+    })
     window.vm = this
   }
 }
---
+++ src/components/AspectQuiz.vue
@@ -0,0 +1,136 @@
+<template>
+  <h2 v-text="$t(`theme_${theme}`)" />
+  <div class="question">
+    <p>
+      Which of these two sides of <i>{{ theme }}</i> do you have the <b>strongest</b> connection to?
+    </p>
+
+    <div class="row">
+      <i v-text="$t(`aspect_desc.${aspect_pair[0]}`)" />
+      <input
+        v-for="i in [0,1,2,3,4]" :key="i"
+        type="radio" v-model="theme_rating" :value="i" />
+      <i v-text="$t(`aspect_desc.${aspect_pair[1]}`)" />
+    </div>
+  </div>
+
+  <div class="question" v-if="aspect_pick">
+    <p>
+      What is your relationship with that theme?
+    </p>
+    <div class="col">
+      <label :key="model" v-for="desc, model in {
+        'user': `You <b>use</b> ${aspect_desc.toLowerCase()}`,
+        'tool': `You <b>are used by</b> ${aspect_desc.toLowerCase()}`,
+        'subject': `You <b>act on</b> ${aspect_desc.toLowerCase()}`,
+        'object': `You <b>are acted on by</b> ${aspect_desc.toLowerCase()}`
+      }">
+        <input type="checkbox"
+          :checked.prop="relationship == model"
+          :disabled="masterclass_nature === true"
+          @click="relationship != model ? relationship = model : relationship = nature = undefined"
+        />
+        <span v-html="desc" />
+      </label>
+      <label :key="model" v-for="desc, model in {
+        'subsumer': `You <b>subsume</b> ${aspect_desc.toLowerCase()}`,
+        'subsumee': `You <b>are subsumed by</b> ${aspect_desc.toLowerCase()}`,
+      }">
+        <input type="checkbox"
+          :checked.prop="relationship == model"
+          :disabled="masterclass_nature === false"
+          @click="relationship != model ? relationship = model : relationship = nature = undefined"
+        />
+        <span v-html="desc" />
+      </label>
+    </div>
+  </div>
+
+  <div class="question" v-if="aspect_pick">
+    <p>
+      ...in a way that is:
+    </p>
+    <div class="col">
+      <label :key="model" v-for="desc, model in {
+        'intrinsic': `Intrinsic, introverted, advancing one's own goals. Advancing my goals advances the narrative's goals.`,
+        'extrinsic': `Extrinsic, extroverted, advancing the goals of others. I advance the goals of others to advance the narrative's goals.`,
+        'victim': `For/at the expense of the narrative, or a higher power. The narrative advances its goals by actively acting in my detriment.`
+      }">
+        <input type="checkbox"
+          :checked.prop="nature == model"
+          :disabled="masterclass_rel === true"
+          @click="nature != model ? nature = model : nature = undefined"
+        />
+        {{ desc }}
+      </label>
+      <label :key="model" v-for="desc, model in {
+        'self': `For both the self and the narrative as one, inseperable. My goals and the narrative's are inseperable. We are codependent on each other.`
+      }">
+        <input type="checkbox"
+          :checked.prop="nature == model"
+          :disabled="masterclass_rel === false"
+          @click="nature != model ? nature = model : nature = undefined"
+        />
+        {{ desc }}
+      </label>
+    </div>
+  </div>
+
+  <pre v-text="{theme_rating, relationship, nature, aspect_pick, score_rating, class_pick}" />
+  <div v-if="aspect_pick && class_pick">
+    <span v-text="confidence" /> <span v-text="class_pick" /> of <span v-text="aspect_pick" />
+  </div>
+</template>
+
+<script>
+
+import data from "@/data.yaml";
+
+export default {
+  name: 'AspectQuiz',
+  props: [ 'theme' ],
+  emits: [ 'aspectSet', 'classSet', 'confidenceSet' ],
+  data: function() {
+    return {
+      theme_rating: 2,
+      relationship: undefined,
+      nature: undefined,
+    }
+  },
+  watch: {
+    aspect_pick(to) {
+      this.$emit('aspectSet', to)
+    },
+    class_pick(to) {
+      this.$emit('classSet', to)
+    },
+    confidence(to) {
+      this.$emit('confidenceSet', to)
+    },
+  },
+  computed: {
+    aspect_pair() { return data.aspect_pairs[this.theme] || ['Null', 'Nil'] },
+    aspect_pick() { return this.score_rating > 0 ? this.aspect_pair[Math.round(this.theme_rating/5)] : undefined },
+    aspect_desc() { return this.$t(`aspect_desc.${this.aspect_pick}`) },
+    score_rating() { return Math.abs(this.theme_rating-2) },
+    class_pick() {
+      var nature_opts = data.class_nature[this.nature] || []
+      var rel_opts = data.class_rel[this.relationship] || []
+      var filtered = nature_opts.filter(a => rel_opts.includes(a))
+      console.log(rel_opts, nature_opts, filtered)
+      return filtered[0] || "Lack"
+    },
+    masterclass_nature() {
+      if (this.nature == "self") return true
+      else if (this.nature) return false
+      return undefined
+    },
+    masterclass_rel() {
+      if (this.relationship && this.relationship.startsWith("subsume")) return true
+      else if (this.relationship) return false
+      return undefined
+    },
+    confidence() { return this.score_rating }
+  },
+}
+</script>
